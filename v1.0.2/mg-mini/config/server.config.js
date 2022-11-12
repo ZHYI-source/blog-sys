1c92b34b32ec38011dbfde07b644fa5b99e4dcc5
Tom <1840354092@qq.com>
2022-11-12 16:36:42 +0800

    🎈 新增 我的世界模块

--- v1.0.2/mg-mini/config/server.config.js
+++ v1.0.2/mg-mini/config/server.config.js
@@ -4,8 +4,7 @@
  *@Description:服务主机配置
  */
  const config = {
-    SERVER_URL: "https://zhouyi.run:5230",//正式环境
-    // SERVER_URL: "https://localhost:5230",//开发环境
+    SERVER_URL: "https://localhost:5230",//开发环境
 };
 
 export default config
